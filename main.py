61a6bccd38b39549e7a1490ced96a16fc9af65a7
harelfogel <fogell06@gmail.com>
2023-04-14 22:45:04 +0300

    adding some primery data

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify
-import baysian_model_script
-from generate_dataset import append_data_to_csv
 import pandas as pd
+import numpy as np
+from generate_dataset import append_data_to_csv
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import baysian_model_script
+
 
 app = Flask(__name__)
 CORS(app)
@@ -19,7 +21,7 @@ def recommend_device_api():
 
 @app.route('/update_data', methods=['POST'])
 def update_data_api():
-    data = request.get_json()   
+    data = request.get_json()
     append_data_to_csv(data, "mock_data.csv")
     return jsonify({"status": "success", "message": "Data updated successfully"})
 
@@ -29,26 +31,38 @@ def get_devices():
         {"device": "ac_energy", "name": "AC"},
         {"device": "heater_energy", "name": "Heater"},
         {"device": "laundry_energy", "name": "Laundry Machine"},
+        {"device": "lights_energy", "name": "Lights"},
+        {"device": "fan_energy", "name": "Fan"},
     ]
     return jsonify(devices)
 
+
+def calculate_total_energy(df, devices):
+    total_energy = np.zeros(len(df))
+    for device in devices:
+        total_energy += df[device].sum()
+
+    return total_energy.tolist()
 
 
 @app.route('/graph-data', methods=['GET'])
 def get_graph_data():
     device = request.args.get('device', 'ac_energy')
+    if not device:
+        device='ac_energy'
     time_range = request.args.get('time_range', 'daily')
     df = pd.read_csv('mock_data.csv')
-    if time_range == 'daily':
-        df_grouped = df.groupby(pd.to_datetime(df['timestamp']).dt.date)[device].sum()
-    elif time_range == 'monthly':
-        df_grouped = df.groupby(pd.to_datetime(df['timestamp']).dt.to_period('M'))[device].sum()
-    elif time_range == 'yearly':
-        df_grouped = df.groupby(pd.to_datetime(df['timestamp']).dt.to_period('Y'))[device].sum()
-    else:
-        df_grouped = df.groupby(pd.to_datetime(df['timestamp']).dt.date)[device].sum()
 
-    labels = df_grouped.index.tolist()
+    if time_range == 'daily':
+        df_grouped = df.groupby(pd.to_datetime(df['timestamp']).dt.date)
+    elif time_range == 'monthly':
+        df_grouped = df.groupby(pd.to_datetime(df['timestamp']).dt.to_period('M'))
+    elif time_range == 'yearly':
+        df_grouped = df.groupby(pd.to_datetime(df['timestamp']).dt.to_period('Y'))
+    else:
+        df_grouped = df.groupby(pd.to_datetime(df['timestamp']).dt.date)
+
+    labels = df_grouped.indices.keys()
 
     # Convert Period objects to string format for monthly and yearly time ranges
     if time_range == 'monthly':
@@ -56,7 +70,8 @@ def get_graph_data():
     elif time_range == 'yearly':
         labels = [label.strftime("%Y") for label in labels]
 
-    data = df_grouped.values.tolist()
+    # Calculate total energy consumption for the selected device
+    data = calculate_total_energy(df_grouped, [device])
 
     return jsonify({'labels': labels, 'data': data})
 
